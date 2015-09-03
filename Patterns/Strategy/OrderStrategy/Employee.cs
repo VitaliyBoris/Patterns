3f247853f94f1db4654b11491f9b4e6dc52e741f
Vitaliy Boris <vitaliyboris@gmail.com>
2015-09-03 22:20:31 +0300

    Strategy added

---
+++ Patterns/Strategy/OrderStrategy/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Patterns.Strategy.OrderStrategy
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id = {0}, Name = {1}", Id, Name);
+        }
+
+        public static void SortLists()
+        {
+            var list = new List<Employee>();
+
+            list.Sort(new EmployeeByIdComparer());
+
+            list.Sort((x,y) => x.Name.CompareTo(y.Name));
+
+            var comparer = new EmployeeByIdComparer();
+
+            var set = new SortedSet<Employee>(comparer);
+
+            var comparer2 = ComparerFactory.Create<Employee>((x, y) => x.Id.CompareTo(y.Id));
+            var set2 = new SortedSet<Employee>(comparer2);
+        }
+    }
+}
---
+++ Patterns/Strategy/OrderStrategy/EmployeeByIdComparer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Patterns.Strategy.OrderStrategy
+{
+    public class EmployeeByIdComparer : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return x.Id.CompareTo(y.Id);
+        }
+    }
+}
