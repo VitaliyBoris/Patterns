343f5f62bbb76517915bbe61f80c62ca846c0e25
Vitaliy Boris <vitaliyboris@gmail.com>
2015-09-03 22:48:33 +0300

    Added Template Method (classic impl)

---
+++ Patterns/TemplateMethod/Classic/LogReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Patterns.Common;
+
+namespace Patterns.TemplateMethod.Classic
+{
+    public abstract class LogReader
+    {
+        private int _currentPosition;
+
+        public IEnumerable<LogEntry> ReadLogEntry()
+        {
+            return ReadEntries(ref _currentPosition).Select(ParseLogEntry);
+        }
+
+        protected abstract IEnumerable<string> ReadEntries(ref int currentPosition);
+
+        protected abstract LogEntry ParseLogEntry(string stringEntry);
+    }
+}
