28954aff1c779adb896aaba68226fa59df58f9ca
Vitaliy Boris <vitaliyboris@gmail.com>
2015-09-03 23:56:37 +0300

    DelegateBased Template Method added

---
+++ Patterns/TemplateMethod/DelegateBased/ILogSaver.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Patterns.Common;
+
+namespace Patterns.TemplateMethod.DelegateBased
+{
+    public interface ILogSaver
+    {
+        void UploadLogEntries(IEnumerable<LogEntry> logEntries);
+
+        void UploadExceptions(IEnumerable<ExceptionLogEntry> exceptions);
+    }
+}
---
+++ Patterns/TemplateMethod/DelegateBased/LogSaverProxy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using Patterns.Common;
+
+namespace Patterns.TemplateMethod.DelegateBased
+{
+    public class LogSaverProxy : ILogSaver
+    {
+        class LogSaverClient : ClientBase<ILogSaver>
+        {
+            public ILogSaver LogSaver
+            {
+                get { return Channel; }
+            }
+        }
+
+        public void UploadLogEntries(IEnumerable<LogEntry> logEntries)
+        {
+            UseProxyClient(c => c.UploadLogEntries(logEntries));
+        }
+
+        public void UploadExceptions(IEnumerable<ExceptionLogEntry> exceptions)
+        {
+            UseProxyClient(c => c.UploadExceptions(exceptions));
+        }
+
+        private void UseProxyClient(Action<ILogSaver> accessor)
+        {
+            var client = new LogSaverClient();
+
+            try
+            {
+                accessor(client.LogSaver);
+                client.Close();
+            }
+            catch (CommunicationException)
+            {
+                client.Abort();
+                throw;
+            }
+        }
+    }
+}
